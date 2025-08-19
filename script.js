0adccd49e631ecd15d3f383af5f234bf57ccd315
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-08-19 13:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function wait (ms){
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
-function async showmessage(){
+ async function showmessage(){
 	 let text = document.getElementById("text").value;
 	 let delay = document.getElementById("delay").value;
 	let output = document.getElementById("output");
 	   if(isNaN(delay) || delay<0){
-		    output.textcontent = "please enter a valid delay";
+		    output.textContent = "please enter a valid delay";
 	   }
 	      if(!text){
-		    output.textcontent = "please enter a valid text";
+		    output.textContent = "please enter a valid text";
 	   } 
-	  output.textcontent = "waiting";
-	await(delay);
-	output.textcontent = text;
+	  output.textContent = "waiting";
+	await wait(delay);
+	output.textContent = text;
 }
 document.getElementById("btn").addEventListener("click",showmessage);
